f101de242bc18ca9e8f00954a88cc8bc9f4a76ad
maryamTGN <112436910+MaryTGN@users.noreply.github.com>
2022-08-30 21:48:03 +0000

    Dictionary Practice.p1

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 programming_dictionary = {
-  "Bug": "An error in a program that prevents the program from running as expected.",
-  "Function": "A piece of code that you can easily call over and over again."
+    "Bug":
+    "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."
 }
 
 # Retrieving items from dictionary
 print(programming_dictionary["Bug"])
 
 # Adding new item to dictionary
-programming_dictionary ["Loop"] = "The action of doing something over and over again."
+programming_dictionary[
+    "Loop"] = "The action of doing something over and over again."
 print(programming_dictionary)
 
 # Creating an empty dictionary
@@ -23,9 +25,8 @@ print(programming_dictionary)
 
 # Loop through the dictionary
 for thing in programming_dictionary:
-  print(thing)
+    print(thing)
 
 for key in programming_dictionary:
-  print(key)
-  print(programming_dictionary[key])
-  
+    print(key)
+    print(programming_dictionary[key])
