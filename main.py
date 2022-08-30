dd9e8d41a602d176eb6da6ad5a9b92bb72ea1887
maryamTGN <112436910+MaryTGN@users.noreply.github.com>
2022-08-30 21:37:54 +0000

    Dictionary practice

--- main.py
+++ main.py
@@ -1,73 +1,31 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+programming_dictionary = {
+  "Bug": "An error in a program that prevents the program from running as expected.",
+  "Function": "A piece of code that you can easily call over and over again."
+}
 
-# def caesar(start_text, shift_amount, cipher_direction):
-#   end_text = ""
-#   if cipher_direction == "decode":
-#     shift_amount *= -1
-#   for char in start_text:
-#     if char in alphabet:
-#       position = alphabet.index(char)
-#       new_position = position + shift_amount
-#       end_text += alphabet[new_position]
-#     else: 
-#       end_text += char
-      
-    #TODO-3: What happens if the user enters a number/symbol/space?
-    #Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-    #e.g. start_text = "meet me at 3"
-    #end_text = "•••• •• •• 3"
-    
-    
-  # print(f"Here's the {cipher_direction}d result: {end_text}")
+# Retrieving items from dictionary
+print(programming_dictionary["Bug"])
 
-#TODO-1: Import and print the logo from art.py when the program starts.
-# from art import logo
-# print(logo)
+# Adding new item to dictionary
+programming_dictionary ["Loop"] = "The action of doing something over and over again."
+print(programming_dictionary)
 
-#TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-#e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-#If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-#Hint: Try creating a while loop that continues to execute the program if the user types 'yes'. 
+# Creating an empty dictionary
+empty_dictionary = {}
 
-from art import logo
-print(logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    if char in alphabet:
-      position = alphabet.index(char)
-      shift_amount = shift_amount % 26
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else: 
-      end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+# Wipe an existing distionary
+# programming_dictionary = {}
+# print(programming_dictionary)
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+# Edit an item in dictionary
+programming_dictionary["Bug"] = "A moth in your computer."
+print(programming_dictionary)
 
+# Loop through the dictionary
+for thing in programming_dictionary:
+  print(thing)
 
-restart = input("type 'Y' to continue and type 'N' to stop.").lower()
-while restart == "y":
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  restart = input("type 'Y' to continue and type 'N' to stop.").lower()
-
-print("BYE")
+for key in programming_dictionary:
+  print(key)
+  print(programming_dictionary[key])
   
-
-
-#TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-#Try running the program and entering a shift number of 45.
-#Add some code so that the program continues to work even if the user enters a shift number greater than 26. 
-#Hint: Think about how you can use the modulus (%).
-
-
-
-
